d5060ba9492f73f8e2365b5ad629a1f5a66d752e
肆叶 <lofreer@gmail.com>
2021-05-24 16:28:07 +0800

    包名修改

--- test/webview_flutter_test.dart
+++ test/webview_flutter_test.dart
@@ -10,8 +10,8 @@ import 'package:flutter/src/foundation/basic_types.dart';
 import 'package:flutter/src/gestures/recognizer.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:webview_flutter/platform_interface.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_full/platform_interface.dart';
+import 'package:webview_flutter_full/webview_flutter.dart';
 
 typedef void VoidCallback();
 
